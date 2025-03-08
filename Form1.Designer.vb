85aa327f6d93e70f71edf0ef01b608e5a80f0132
JadaDev <JadaDev@users.noreply.github.com>
2025-03-08 04:39:18 +0100

    Update Form1.Designer.vb
    v0.0.0.2 Updated
    
    Browse Option Added
    Changed ListView to DataGridView
    Additional Fixes.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -24,7 +24,6 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
@@ -37,27 +36,15 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.CheckBox4 = New System.Windows.Forms.CheckBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
         Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
         Me.Button4 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.bfilelocation = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'ListBox1
-        '
-        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer))
-        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.ListBox1.Font = New System.Drawing.Font("Trebuchet MS", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ListBox1.ForeColor = System.Drawing.Color.Lime
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 18
-        Me.ListBox1.Location = New System.Drawing.Point(0, 86)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(845, 198)
-        Me.ListBox1.TabIndex = 0
         '
         'Button1
         '
@@ -68,7 +55,7 @@ Partial Class Form1
         Me.Button1.ForeColor = System.Drawing.Color.Lime
         Me.Button1.Location = New System.Drawing.Point(0, 0)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(845, 33)
+        Me.Button1.Size = New System.Drawing.Size(829, 33)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Basic Check"
         Me.Button1.UseVisualStyleBackColor = False
@@ -82,7 +69,7 @@ Partial Class Form1
         Me.Button2.ForeColor = System.Drawing.Color.Red
         Me.Button2.Location = New System.Drawing.Point(0, 33)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(845, 33)
+        Me.Button2.Size = New System.Drawing.Size(829, 33)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Advanced Check [Full]"
         Me.Button2.UseVisualStyleBackColor = False
@@ -95,9 +82,9 @@ Partial Class Form1
         Me.CheckBox1.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.CheckBox1.Font = New System.Drawing.Font("Myanmar Text", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckBox1.ForeColor = System.Drawing.Color.White
-        Me.CheckBox1.Location = New System.Drawing.Point(0, 444)
+        Me.CheckBox1.Location = New System.Drawing.Point(0, 469)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(845, 28)
+        Me.CheckBox1.Size = New System.Drawing.Size(829, 28)
         Me.CheckBox1.TabIndex = 3
         Me.CheckBox1.Text = "Double Click Copies :  [ IP : PORT ]"
         Me.CheckBox1.UseVisualStyleBackColor = True
@@ -108,9 +95,9 @@ Partial Class Form1
         Me.CheckBox2.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.CheckBox2.Font = New System.Drawing.Font("Myanmar Text", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckBox2.ForeColor = System.Drawing.Color.White
-        Me.CheckBox2.Location = New System.Drawing.Point(0, 416)
+        Me.CheckBox2.Location = New System.Drawing.Point(0, 441)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(845, 28)
+        Me.CheckBox2.Size = New System.Drawing.Size(829, 28)
         Me.CheckBox2.TabIndex = 4
         Me.CheckBox2.Text = "Double Click Copies :  [ IP ]"
         Me.CheckBox2.UseVisualStyleBackColor = True
@@ -121,7 +108,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(662, 468)
+        Me.Label1.Location = New System.Drawing.Point(646, 493)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(183, 25)
         Me.Label1.TabIndex = 5
@@ -136,12 +123,14 @@ Partial Class Form1
         'CheckBox3
         '
         Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
         Me.CheckBox3.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.CheckBox3.Font = New System.Drawing.Font("Myanmar Text", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckBox3.ForeColor = System.Drawing.Color.White
-        Me.CheckBox3.Location = New System.Drawing.Point(0, 388)
+        Me.CheckBox3.Location = New System.Drawing.Point(0, 413)
         Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(845, 28)
+        Me.CheckBox3.Size = New System.Drawing.Size(829, 28)
         Me.CheckBox3.TabIndex = 6
         Me.CheckBox3.Text = "Enable Country | Organization Info"
         Me.CheckBox3.UseVisualStyleBackColor = True
@@ -174,7 +163,7 @@ Partial Class Form1
         Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button3.ForeColor = System.Drawing.Color.White
-        Me.Button3.Location = New System.Drawing.Point(701, 416)
+        Me.Button3.Location = New System.Drawing.Point(685, 441)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(101, 30)
         Me.Button3.TabIndex = 10
@@ -184,30 +173,17 @@ Partial Class Form1
         'CheckBox4
         '
         Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Checked = True
+        Me.CheckBox4.CheckState = System.Windows.Forms.CheckState.Checked
         Me.CheckBox4.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.CheckBox4.Font = New System.Drawing.Font("Myanmar Text", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckBox4.ForeColor = System.Drawing.Color.White
-        Me.CheckBox4.Location = New System.Drawing.Point(0, 472)
+        Me.CheckBox4.Location = New System.Drawing.Point(0, 497)
         Me.CheckBox4.Name = "CheckBox4"
-        Me.CheckBox4.Size = New System.Drawing.Size(845, 28)
+        Me.CheckBox4.Size = New System.Drawing.Size(829, 28)
         Me.CheckBox4.TabIndex = 11
         Me.CheckBox4.Text = "Click For IP Region - Country - Organization Info"
         Me.CheckBox4.UseVisualStyleBackColor = True
-        '
-        'ListBox2
-        '
-        Me.ListBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ListBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer))
-        Me.ListBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.ListBox2.Font = New System.Drawing.Font("Trebuchet MS", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ListBox2.ForeColor = System.Drawing.Color.Lime
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.ItemHeight = 18
-        Me.ListBox2.Location = New System.Drawing.Point(0, 290)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(751, 90)
-        Me.ListBox2.TabIndex = 13
         '
         'Timer3
         '
@@ -218,11 +194,11 @@ Partial Class Form1
         Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button4.ForeColor = System.Drawing.Color.Red
-        Me.Button4.Location = New System.Drawing.Point(755, 290)
+        Me.Button4.Location = New System.Drawing.Point(739, 260)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(88, 90)
+        Me.Button4.Size = New System.Drawing.Size(88, 76)
         Me.Button4.TabIndex = 14
-        Me.Button4.Text = "CLOSE " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PROCESS"
+        Me.Button4.Text = "Close"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'TextBox1
@@ -232,21 +208,63 @@ Partial Class Form1
         Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.TextBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.Network_Finder.My.MySettings.Default, "APIKEY", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
         Me.TextBox1.ForeColor = System.Drawing.Color.White
-        Me.TextBox1.Location = New System.Drawing.Point(701, 452)
+        Me.TextBox1.Location = New System.Drawing.Point(685, 477)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(101, 13)
         Me.TextBox1.TabIndex = 9
         Me.TextBox1.Text = Global.Network_Finder.My.MySettings.Default.APIKEY
         Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 88)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(824, 166)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView2.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.DataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.GridColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.DataGridView2.Location = New System.Drawing.Point(3, 259)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(730, 148)
+        Me.DataGridView2.TabIndex = 16
+        '
+        'bfilelocation
+        '
+        Me.bfilelocation.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bfilelocation.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bfilelocation.Font = New System.Drawing.Font("Arial Rounded MT Bold", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bfilelocation.ForeColor = System.Drawing.Color.DodgerBlue
+        Me.bfilelocation.Location = New System.Drawing.Point(739, 342)
+        Me.bfilelocation.Name = "bfilelocation"
+        Me.bfilelocation.Size = New System.Drawing.Size(88, 65)
+        Me.bfilelocation.TabIndex = 17
+        Me.bfilelocation.Text = "Browse"
+        Me.bfilelocation.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(845, 500)
+        Me.ClientSize = New System.Drawing.Size(829, 525)
+        Me.Controls.Add(Me.bfilelocation)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label3)
@@ -257,19 +275,18 @@ Partial Class Form1
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.CheckBox4)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Network Finder by JadaDev IPINFO | V0.0.0.2"
         Me.TopMost = True
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents CheckBox1 As CheckBox
@@ -283,7 +300,9 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button3 As Button
     Friend WithEvents CheckBox4 As CheckBox
-    Friend WithEvents ListBox2 As ListBox
     Friend WithEvents Timer3 As Timer
     Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents bfilelocation As Button
 End Class
